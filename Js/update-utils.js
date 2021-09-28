4cf1ff46525b5cd5c2bd51b204e94c5a1ea57b19
arthurcoelho442 <arthurcoelho442@gmail.com>
2021-09-27 23:38:11 -0300

    .

--- Js/update-utils.js
+++ Js/update-utils.js
@@ -234,7 +234,7 @@ export const updateTroops = (scene, level, time, delta, wave, waveSpeed) => {
             } else if (pos.x <= 125 && pos.y <= 600) {
                 sprite.setVelocityX(0);
                 sprite.setVelocityY(velocidade);
-            } if (pos.x >= 120 && pos.x <= 430 && pos.y >= 225 && pos.y <= 230) {
+            } if (pos.x <= 125 && pos.y >= 225 && pos.y <= 230) {
                 sprite.rotation += rotation;
                 sprite.setVelocityX(velocidade);
                 sprite.setVelocityY(0);
